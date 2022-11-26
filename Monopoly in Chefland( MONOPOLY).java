ef09807c3ef856a18d1e02d050ca4aaabe6c39e4
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-11-26 21:23:20 +0530

    Update Monopoly in Chefland( MONOPOLY).java

--- Monopoly in Chefland( MONOPOLY).java
+++ Monopoly in Chefland( MONOPOLY).java
@@ -1,1 +1,30 @@
-<a>https://www.codechef.com/START66D</a>
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int t=obj.nextInt();
+		while(t>0)
+		{
+		    int r1=obj.nextInt();
+		    int r2=obj.nextInt();
+		    int r3=obj.nextInt();
+		    if(r1>(r2+r3) || r2>(r1+r3) || r3>(r1+r2))
+		    {
+		        System.out.println("Yes");
+		    }
+		    else
+		    {
+		        System.out.println("No");
+		    }
+		    t--;
+		}
+	}
+}
