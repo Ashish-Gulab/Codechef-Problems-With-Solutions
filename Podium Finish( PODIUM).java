af594bb37ecc64fcc81af0f68a1c1087ed069b37
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-11-27 12:17:53 +0530

    Create Podium Finish( PODIUM).java

---
+++ Podium Finish( PODIUM).java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int t=obj.nextInt();
+		while(t>0)
+		{
+		    int n1=obj.nextInt();
+		    int n2=obj.nextInt();
+		    System.out.println(n1+n2);
+		    t--;
+		}
+	}
+}
