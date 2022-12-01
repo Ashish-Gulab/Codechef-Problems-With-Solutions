f2cfc74925debbb68ab2152483d98829f52e9c7f
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-01 20:17:39 +0530

    Create Course Registration( COURSEREG).java

---
+++ Course Registration( COURSEREG).java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int t=obj.nextInt();
+		while(t>0)
+		{
+		    int a=obj.nextInt();
+		    int b=obj.nextInt();
+		    int c=obj.nextInt();
+		    if((a+c)<=b)
+		    {
+		        System.out.println("Yes");
+		    }
+		    else
+		    {
+		        System.out.println("No");
+		    }
+		    t--;
+		}
+	}
+}
