aecb35716f49588413077a555df97b7b9582340e
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-01 20:14:54 +0530

    Create Fever( FEVER).java

---
+++ Fever( FEVER).java
@@ -0,0 +1,27 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int n=obj.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    int n1=obj.nextInt();
+		    if(n1>98)
+		    {
+		        System.out.println("YES");
+		    }
+		    else
+		    {
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
