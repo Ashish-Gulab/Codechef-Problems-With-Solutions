776dc99395514dedc26ea29488a92d3d0d5fe176
Ashish Gulab <ashishgulab.gulab@gmail.com>
2022-12-26 23:20:33 +0530

    Create Cars and Bikes(TYRES).java

---
+++ Cars and Bikes(TYRES).java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner obj=new Scanner(System.in);
+		int t=obj.nextInt();
+		while(t>0)
+		{
+		    int n=obj.nextInt();
+		    if(n%4==0)
+		    {
+		        System.out.println("NO");
+		    }
+		    else
+		    {
+		        System.out.println("YES");
+		    }
+		    t--;
+		}
+	}
+}
