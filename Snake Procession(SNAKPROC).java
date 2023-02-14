8adea9e61f3382b4199c77f12b9b964fccb767c0
Ashish Gulab <ashishgulab.gulab@gmail.com>
2023-02-14 23:24:53 +0530

    Update Snake Procession(SNAKPROC).java

--- Snake Procession(SNAKPROC).java
+++ Snake Procession(SNAKPROC).java
@@ -9,6 +9,36 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		// your code goes here
+			Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		
+		while (t-- > 0) {
+			in.nextInt();
+		    in.nextLine();
+		    String snakes = in.nextLine();
+		    snakes = snakes.replace(".", "");
+		    snakes = snakes.replace("HT", "");
+		    if (snakes == "") System.out.println("Valid");
+		    else System.out.println("Invalid");
+		}
+	}
+}
+
+
+
+/*  Wrong Answer Further improvement is required
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
 		Scanner obj=new Scanner(System.in);
 		StringBuilder s=new StringBuilder();
 		int r=obj.nextInt();
@@ -59,3 +89,4 @@ class Codechef
 		}
 	}
 }
+*/
